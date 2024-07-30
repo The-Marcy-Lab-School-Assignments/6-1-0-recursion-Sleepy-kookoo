ebfb1d0da23e01f44eea2ae45ffacfc5684ac949
Sleepy-kookoo <americaxalvarez@gmail.com>
2024-07-30 09:47:46 -0400

    Answered short-answers

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -8,15 +8,36 @@ const sum = (arr, total = 0, idx = 0) => {
 }
 
 // Reverse string using recursive approach
-const reverse = (str) => { };
+const reverse = (str) => {
+
+};
 
 // Iterative approach to solving the fibonacci problem
-const fibIter = (n) => { };
+const fibIter = (n) => {
+
+};
 
 // Recursive fibonacci
 const fibRec = (n) => { };
 
+
+const binarySearchIn = (arr, target) => {
+  let start = 0, end = arr.length - 1 // parameters
+  while (start <= end) { //if start > ebd return -1 
+    const mid = Math.floor((start + end) / 2)
+    if (arr[mid] === target) return mid // if found item 
+    if (arr[mid] > target) end = mid - 1 // rec case
+    if (arr[mid] < target) start = mid + 1 // rec case
+  }
+  return -1
+};
+
 // Return the index of target in arr, or -1 if not found using recursion 
-const binarySearch = (arr, target, start = 0, end = arr.length - 1) => { };
+const binarySearch = (arr, target, start = 0, end = arr.length - 1) => {
+  const mid = Math.floor((start + end) / 2)
+
+
+  return binarySearch()
+};
 
 module.exports = { sum, reverse, fibRec, fibIter, binarySearch };
--- src/playground.js
+++ src/playground.js
@@ -9,10 +9,18 @@
 // console.log(countDownFromNRecursive(5))
 
 
-const sum = (arr, total = 0, idx = 0) => {
-  if (idx === arr.length) return total
+// const sum = (arr, total = 0, idx = 0) => {
+//   if (idx === arr.length) return total
 
-  return sum(arr, total + arr[idx], idx + 1)
-}
+//   return sum(arr, total + arr[idx], idx + 1)
+// }
 
-console.log(sum([1, 2, 3]))
+// console.log(sum([1, 2, 3]))
+
+const reverse = (str) => {
+  for (let i = str; i = 0; i++) {
+    consoe
+  }
+};
+
+console.log(reverse('hello'))
